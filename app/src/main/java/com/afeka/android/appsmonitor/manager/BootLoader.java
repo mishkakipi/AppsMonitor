841fc2ea21c67696b06c05c0ff48a57c18a825b6
Michael <mishka.kipi@gmail.com>
2016-07-16 17:09:14 +0300

    Michaelk17 initial commit

--- app/src/main/java/com/afeka/android/appsmonitor/manager/BootLoader.java
+++ app/src/main/java/com/afeka/android/appsmonitor/manager/BootLoader.java
@@ -1,7 +1,30 @@
 package com.afeka.android.appsmonitor.manager;
 
+import android.app.Application;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.widget.Toast;
+
 /**
  * Created by Michael on 16/07/2016.
  */
-public class BootLoader {
+public class BootLoader extends Application {
+    public static final String PREFS_NAME = "appsmonitor.properties";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        String mode = settings.getString("mode","");
+        switch (mode) {
+            case "child":
+                Toast.makeText(getBaseContext(), "Child mode", Toast.LENGTH_LONG).show();
+                break;
+            case "parent":
+                Toast.makeText(getBaseContext(), "Parent mode", Toast.LENGTH_LONG).show();
+                break;
+            default:
+                Toast.makeText(getBaseContext(), "Registration mode", Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
 }
